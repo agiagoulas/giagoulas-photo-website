0b4ec8bac2a52aaa6b54bdb314189fdea579231f
Alexander Giagoulas <agiagoulas@AGMPBII.fritz.box>
2022-04-17 20:12:08 +0200

    initial commit

---
+++ footer-single.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying the footer for single posts.
+ *
+ * @package Timber Lite
+ * @since Timber 1.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ){
+	exit; // Exit if accessed directly
+} ?>
+<footer id="colophon" class="site-footer  site-footer--single">
+	<div class="site-info c-footer__copyright-text">
+		<span class="c-footer__credits">Copyright 2021</span>
+		<span class="sep"> | </span>
+		<span class="c-footer__credits">Alexander Giagoulas</span> 
+    </div>
+</footer><!-- #colophon -->
